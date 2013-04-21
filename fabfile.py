5071121404b8ffcf1194b4bbaa257cb9d831f8c9
Mathieu Turcotte <turcotte.mat@gmail.com>
2013-04-21 10:55:31 -0400

    Pep8 cleanup.

--- fabfile.py
+++ fabfile.py
@@ -6,34 +6,43 @@ from fabric.utils import *
 from fabric.colors import *
 from fabric.contrib.files import exists
 
-USER="ipupdate"
-LOG_DIR="/var/log/ipupdate"
-ETC_DIR="/usr/local/etc"
-BIN_DIR="/usr/local/bin"
+USER = "ipupdate"
+LOG_DIR = "/var/log/ipupdate"
+ETC_DIR = "/usr/local/etc"
+BIN_DIR = "/usr/local/bin"
+
 
 def chown(path):
     sudo("chown -R %s:%s %s" % (USER, USER, path))
+
 
 def add_user():
     with settings(hide("warnings"), warn_only=True):
         sudo('adduser --system --no-create-home %s' % USER)
 
+
 def configure_logging():
     sudo("mkdir -p %s" % LOG_DIR)
     chown(LOG_DIR)
+
 
 def install_config(config_file):
     put(config_file, "%s/ipupdate.conf" % ETC_DIR, use_sudo=True, mode=0644)
     chown("%s/ipupdate.conf" % ETC_DIR)
 
+
 def install_service():
     if exists('/etc/fedora-release'):
-        put("systemd/ipupdate.service", "/lib/systemd/system/ipupdate.service", use_sudo=True, mode=0644)
+        put("systemd/ipupdate.service",
+            "/lib/systemd/system/ipupdate.service", use_sudo=True, mode=0644)
     else:
-        put("upstart/ipupdate.conf", "/etc/init/ipupdate.conf", use_sudo=True, mode=0644)
+        put("upstart/ipupdate.conf", "/etc/init/ipupdate.conf", use_sudo=True,
+            mode=0644)
+
 
 def install_script():
     put("ipupdate.py", "%s/ipupdate.py" % BIN_DIR, use_sudo=True, mode=0755)
+
 
 @task()
 def install(config_file):
