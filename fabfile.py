5f5c7ecd2000a998dd061548977751faf744e659
Mathieu Turcotte <turcotte.mat@gmail.com>
2013-04-21 10:26:26 -0400

    Extract common directories.

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,10 @@ from fabric.utils import *
 from fabric.colors import *
 from fabric.contrib.files import exists
 
+LOG_DIR="/var/log/ipupdate"
+ETC_DIR="/usr/local/etc"
+BIN_DIR="/usr/local/bin"
+
 def add_user(username):
     sudo('adduser --system --no-create-home %s' % username)
 
@@ -16,12 +20,12 @@ def install_python3(is_fedora):
         sudo('apt-get --assume-yes install python3')
 
 def configure_logging():
-    sudo("mkdir -p /var/log/ipupdate")
-    sudo("chown -R ipupdate:ipupdate /var/log/ipupdate")
+    sudo("mkdir -p %s" % LOG_DIR)
+    sudo("chown -R ipupdate:ipupdate %s" % LOG_DIR)
 
 def install_conf_file(config_file):
-    put(config_file, "/usr/local/etc/ipupdate.conf", use_sudo=True, mode=0644)
-    sudo("chown ipupdate:ipupdate /usr/local/etc/ipupdate.conf")
+    put(config_file, "%s/ipupdate.conf" % ETC_DIR, use_sudo=True, mode=0644)
+    sudo("chown ipupdate:ipupdate %s/ipupdate.conf" % ETC_DIR)
 
 def install_init_script(is_fedora):
     if is_fedora:
@@ -30,7 +34,7 @@ def install_init_script(is_fedora):
         put("upstart/ipupdate.conf", "/etc/init/ipupdate.conf", use_sudo=True, mode=0644)
 
 def install_script():
-    put("ipupdate.py", "/usr/local/bin/ipupdate.py", use_sudo=True, mode=0755)
+    put("ipupdate.py", "%s/ipupdate.py" % BIN_DIR, use_sudo=True, mode=0755)
 
 @task()
 def install(config_file):
